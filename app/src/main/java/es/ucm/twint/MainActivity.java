c6084db313a13eee88adddd27b58be2525c443c0
Mario Sanchez <mariosanchezde1998@gmail.com>
2022-03-29 18:42:17 +0200

    Agregar activities VerPerfiles y VerMenu

--- app/src/main/java/es/ucm/twint/MainActivity.java
+++ app/src/main/java/es/ucm/twint/MainActivity.java
@@ -2,6 +2,7 @@ package es.ucm.twint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     EditText etUsername;
     EditText etPassword;
     Button btLogin;
+    Button btVerPerfiles;
+    Button btVerMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         etUsername = (EditText) findViewById(R.id.etUser);
         etPassword = (EditText) findViewById(R.id.etPassword);
         btLogin = (Button) findViewById(R.id.btLogin);
+        btVerPerfiles = (Button) findViewById(R.id.btVerPerfiles);
+        btVerMenu = (Button) findViewById(R.id.btVerMenu);
 
         btLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +34,22 @@ public class MainActivity extends AppCompatActivity {
                 String userName = etUsername.getText().toString();
                 String password = etPassword.getText().toString();
                 Toast.makeText(MainActivity.this, "Hola Mario: " + userName + "@" + password, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        btVerPerfiles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent verPerfiles = new Intent(MainActivity.this, VerPerfiles.class );
+                startActivity(verPerfiles);
+            }
+        });
+
+        btVerMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent verMenu = new Intent(MainActivity.this, VerMenu.class );
+                startActivity(verMenu);
             }
         });
     }
