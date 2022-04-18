a1724164bca9a1d3b8e8c23cfaa8c5090c9ac433
fyssunwenbo <fyssunwenbo@gmail.com>
2022-04-19 00:53:58 +0200

    editar perfil completo 80%
    falta subir foto y video
    y donde guardar

--- app/src/main/java/es/ucm/twint/EditProfileFragment.java
+++ app/src/main/java/es/ucm/twint/EditProfileFragment.java
@@ -2,17 +2,35 @@ package es.ucm.twint;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import es.ucm.mocks.Perfil;
 import es.ucm.twint.databinding.FragmentEditProfileBinding;
 
 public class EditProfileFragment extends Fragment {
-    private FragmentEditProfileBinding binding;
-    Uri imageURI;
+
+    View profileView;
+
+    Perfil perfil;
+
+    EditText etPreferences;
+    EditText etPersonalDescription;
+    EditText etEmail;
+
+    Button btUploadPicture;
+    Button btUploadMultimedia;
+    Button btSave;
+
     public EditProfileFragment() {
         // Required empty public constructor
     }
@@ -21,27 +39,106 @@ public class EditProfileFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-       /* binding.btUploadPictures.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                NavHostFragment.findNavController(LoginFragment.this)
-//                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-                Intent gallery = new Intent();
-                gallery.setType("image/*");
-                gallery.setAction(Intent.ACTION_GET_CONTENT);
-
-
-                startActivityForResult(Intent.createChooser(gallery, "Selecciona una imagen"), 1);
-            }
-        });*/
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        profileView = inflater.inflate(R.layout.fragment_edit_profile, container, false);
+        return profileView;
+    }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-        return inflater.inflate(R.layout.fragment_edit_profile, container, false);
+        etPreferences = (EditText) profileView.findViewById(R.id.et_preferences);
+        etPersonalDescription = (EditText) profileView.findViewById(R.id.et_personal_description
+        );
+        etEmail = (EditText) profileView.findViewById(R.id.et_email);
+
+        btUploadPicture = (Button) profileView.findViewById(R.id.bt_upload_pictures);
+        btUploadMultimedia = (Button) profileView.findViewById(R.id.bt_upload_multimedia);
+        btSave = (Button) profileView.findViewById(R.id.bt_save_changes);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        perfil = new Perfil();
+
+        etPersonalDescription.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                perfil.setPersonalDescription(editable.toString());
+            }
+        });
+
+        etPreferences.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                perfil.setPreferences(editable.toString());
+            }
+        });
+
+        etEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                perfil.setEmail(editable.toString());
+            }
+        });
+
+        btUploadPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a = 0;
+            }
+        });
+
+        btUploadMultimedia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a = 0;
+            }
+        });
+
+        btSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a = 0;
+            }
+        });
     }
 }
