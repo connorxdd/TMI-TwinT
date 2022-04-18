157ec3e2b42a18865943c5e18a4ad26703f8f0bf
fyssunwenbo <fyssunwenbo@gmail.com>
2022-04-19 00:52:40 +0200

    Update Perfil.java
    añadir metodos de perfil

--- app/src/main/java/es/ucm/mocks/Perfil.java
+++ app/src/main/java/es/ucm/mocks/Perfil.java
@@ -5,12 +5,23 @@ import java.util.ArrayList;
 public class Perfil {
     String nombre;
     String imagen;
+    String personalDescription;
+    String preferences;
+    String email;
     ArrayList<String> urlFotos;
+    String picture;
+    ArrayList<String> multimedia;
 
+    public Perfil() {
+    }
     public Perfil(String nombre, String imagen, ArrayList<String> urlFotos) {
         this.nombre = nombre;
         this.imagen = imagen;
         this.urlFotos = urlFotos;
+    }
+
+    public void addMultimedia(String m) {
+        this.multimedia.add(m);
     }
 
     public String getNombre() {
@@ -22,4 +33,44 @@ public class Perfil {
     }
 
     public ArrayList<String> getPhotosProfile() { return this.urlFotos; }
+
+    public String getPersonalDescription() {
+        return personalDescription;
+    }
+
+    public void setPersonalDescription(String personalDescription) {
+        this.personalDescription = personalDescription;
+    }
+
+    public String getPreferences() {
+        return preferences;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPreferences(String preferences) {
+        this.preferences = preferences;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public ArrayList<String> getMultimedia() {
+        return multimedia;
+    }
+
+    public void setMultimedia(ArrayList<String> multimedia) {
+        this.multimedia = multimedia;
+    }
 }
