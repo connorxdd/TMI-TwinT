2478da9d59144eec62b215e5b6544883208fb076
fyssunwenbo <fyssunwenbo@gmail.com>
2022-04-28 02:27:40 +0200

    Update EditProfileFragment.java
    EditPerfil actulizar y falta conect to data base

--- app/src/main/java/es/ucm/twint/EditProfileFragment.java
+++ app/src/main/java/es/ucm/twint/EditProfileFragment.java
@@ -8,8 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultCallback;
@@ -25,7 +29,7 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
-import java.util.UUID;
+import java.util.Set;
 
 import es.ucm.mocks.Perfil;
 
@@ -38,10 +42,14 @@ public class EditProfileFragment extends Fragment {
 
     EditText etPreferences;
     EditText etPersonalDescription;
-    EditText etEmail;
+    EditText etBiogrphy;
+
+    TextView tvSocialNetwork;
+    Boolean bSocialNetworkDropDownIsOn;
+
+    TableLayout tlSocialNetworks;
 
     Button btUploadPicture;
-    Button btUploadMultimedia;
     Button btSave;
     private Uri myImage;
     private ImageView mImageView;
@@ -67,12 +75,14 @@ public class EditProfileFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         etPreferences = (EditText) profileView.findViewById(R.id.et_preferences);
-        etPersonalDescription = (EditText) profileView.findViewById(R.id.et_personal_description
-        );
-        etEmail = (EditText) profileView.findViewById(R.id.et_email);
+        etPersonalDescription = (EditText) profileView.findViewById(R.id.et_personal_description);
+        etBiogrphy = (EditText) profileView.findViewById(R.id.et_biography);
+        tvSocialNetwork = (TextView) profileView.findViewById(R.id.tv_social_network);
+        bSocialNetworkDropDownIsOn = false;
+        tlSocialNetworks = (TableLayout) profileView.findViewById(R.id.tl_social_networks);
+        setTvSocialNetworkDropDown(bSocialNetworkDropDownIsOn);
 
         btUploadPicture = (Button) profileView.findViewById(R.id.bt_upload_pictures);
-        btUploadMultimedia = (Button) profileView.findViewById(R.id.bt_upload_multimedia);
         btSave = (Button) profileView.findViewById(R.id.bt_save_changes);
         mImageView = profileView.findViewById(R.id.profile_image);
         storage = FirebaseStorage.getInstance();
@@ -118,7 +128,7 @@ public class EditProfileFragment extends Fragment {
             }
         });
 
-        etEmail.addTextChangedListener(new TextWatcher() {
+        etBiogrphy.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
@@ -131,7 +141,7 @@ public class EditProfileFragment extends Fragment {
 
             @Override
             public void afterTextChanged(Editable editable) {
-                perfil.setEmail(editable.toString());
+                perfil.setBiography(editable.toString());
             }
         });
 
@@ -141,12 +151,6 @@ public class EditProfileFragment extends Fragment {
                 mGetContent.launch("image/*");
             }
         });
-        btUploadMultimedia.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int a = 0;
-            }
-        });
 
         btSave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -154,15 +158,71 @@ public class EditProfileFragment extends Fragment {
                 int a = 0;
             }
         });
+
+        tvSocialNetwork.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bSocialNetworkDropDownIsOn = !bSocialNetworkDropDownIsOn;
+                setTvSocialNetworkDropDown(bSocialNetworkDropDownIsOn);
+            }
+        });
+
+        Set<String> keys = perfil.getSocialNetworks().keySet();
+        for ( String key : keys ) {
+            TableRow row = new TableRow(getActivity());
+            CheckBox checkBox = new CheckBox(getActivity());
+            checkBox.setClickable(false);
+            TextView textView = new TextView(getActivity());
+            EditText editText = new EditText(getActivity());
+
+            String value = perfil.getSocialNetwork(key);
+            checkBox.setChecked(!(value == null || value.isEmpty()));
+            textView.setText(key);
+            editText.setText(value == null ? "" : value);
+
+            editText.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable editable) {
+                    String editedText = editable.toString();
+                    perfil.setSocialNetwork(key, editedText);
+                    checkBox.setChecked(!editedText.isEmpty());
+                }
+            });
+
+            row.addView(checkBox);
+            row.addView(textView);
+            row.addView(editText);
+
+            tlSocialNetworks.addView(row);
+
+        }
+
+    }
+
+    private void setTvSocialNetworkDropDown(Boolean isOn) {
+        tvSocialNetwork.setCompoundDrawablesWithIntrinsicBounds(0, 0, isOn ? android.R.drawable.arrow_up_float : android.R.drawable.arrow_down_float, 0);
+        tlSocialNetworks.setVisibility(isOn ? View.VISIBLE : View.GONE);
     }
 
     private void uploadImage() {
         if(myImage != null){
-            StorageReference reference = storage.getReference().child("images/" + UUID.randomUUID().toString());
+            String imagePath = "images/" + perfil.getId();
+            StorageReference reference = storage.getReference().child(imagePath);
             reference.putFile(myImage).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
                 @Override
                 public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
                     if(task.isSuccessful()){
+                        perfil.setPicture(imagePath);
                         Toast.makeText(getActivity(), "Imagen subida", Toast.LENGTH_SHORT).show();
                     }
                     else{
